cdacc8282e191fe79d19309765e7c5888c795c8b
Vitor Bigelli <vitorbigelli@hotmail.com>
2020-08-17 00:34:17 -0300

    Add sanarflix project

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,60 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Vitor Bigelli', // e.g: 'Name | Developer'
+  lang: 'pt', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: "Hi, I'm",
+  name: 'Vitor Bigelli',
+  subtitle: 'Full-Stack Web Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'Programador desde os 16 anos, possuo graduação técnica em Informática para Internet pela ETEC de Itanhaém e Nanodegrees pela Udacity em Desenvolvimento Front-End e React. Atualmente sou Desenvolvedor Full-Stack e Pesquisador Cientista na Nindoo AI.',
+  paragraphTwo:
+    'Entusiasta da Inteligência Artificial e apaixonado por aprender e ensinar, acredito na tecnologia como ferramenta para construir um mundo melhor.',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project_1.png',
+    title: 'Raras Q&A',
+    info: 'at Nindoo AI',
+    info2:
+      'Desenvolvimento Full-Stack do Web App do maior grafo relacionado a doenças raras. Raras.org é uma iniciativa de ciência aberta para trazer avanços na inteligência artificial e encontrar respostas para doenças raras.',
+    url: 'http://beta.raras.org/',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project_2.png',
+    title: 'DeepWild',
+    info: 'at Nindoo AI',
+    info2:
+      'Desenvolvimento Full-Stack do Web App DeepWild, uma ferramenta que auxilia na conservação e monitoramento de espécies selvagens. Ela utiliza uma inteligência artificial que identifica e classifica os animais por meio de fotos e vídeos. ',
+    url: 'https://www.deepwild.com.br/',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'project_3.png',
+    title: 'Caption Editor - SanarFlix',
+    info: 'at Nindoo AI',
+    info2:
+      'Desenvolvimento Full-Stack do Web App de edição de legendas da plataforma de stream de medicina SanarFlix.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +63,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'bigellivitor@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +72,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      url: 'http://twitter.com/bigellivitor',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/vitor-bigelli-559380150/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/VitorBigelli',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
